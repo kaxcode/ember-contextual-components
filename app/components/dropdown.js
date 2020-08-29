6416935c93f626bd8042d1553825168f8d0d67f8
kenia <keniavalladarez@gmail.com>
2020-08-29 18:39:28 -0400

    Move state and action to controller

---
+++ app/components/dropdown.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class DropdownComponent extends Component {
+  @tracked isOpen = false;
+
+  @action
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+}
